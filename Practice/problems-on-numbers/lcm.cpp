473105df97ac1e18cb3a5d2979f2d5b1874833ba
arvindchauhan1 <arvindchn1@gmail.com>
2024-05-05 12:49:15 +0530

    lcm

---
+++ Practice/problems-on-numbers/lcm.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (a < b)
+        swap(a, b);
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return (a * b) / gcd(a, b);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    cout << "lcm = " << lcm(a, b);
+
+    return 0;
+}
